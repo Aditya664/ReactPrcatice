9d4d40d0ac0c28b7bc7544f2ae46fe569571bfab
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-02-28 10:29:04 +0000

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
-  return (
-    <h1>App</h1>
-  )
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar>
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Navbar>
+    </Box>
+  </BrowserRouter>
 }
 
 export default App;
