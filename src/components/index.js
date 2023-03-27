c3c31e322434d37d9ec43e55ab4276f70b8ee1dc
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-03-27 08:36:40 +0000

    CREATED NEW CLASS TO UNDERSTAND CLASSES IN JAVASCRIPT

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchBar } from './SearcBar';
 export { default as Home } from './home';
-export { default as Props } from './props';
+export { default as Props } from './props'; 
 export { default as Message } from './Message';
 export { default as Counter } from './Counter';
 export { default as Distruct } from './Distruct';
